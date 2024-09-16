c34d5ea8404d2930989ed26be5754ea2a76e380e
Thug-Okami <m.senna.cordeiro@gmail.com>
2024-09-16 11:58:27 -0300

    mudanças na folha de pagamento

--- Calculadora IMC/frmFolhaPagamento.Designer.cs
+++ Calculadora IMC/frmFolhaPagamento.Designer.cs
@@ -29,22 +29,22 @@ namespace Calculadora_IMC
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblSalarioBruto = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.rbtClassC = new System.Windows.Forms.RadioButton();
+            this.rbtClassB = new System.Windows.Forms.RadioButton();
+            this.rbtClassA = new System.Windows.Forms.RadioButton();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.txtSalarioBruto = new System.Windows.Forms.TextBox();
-            this.txtSBDemo = new System.Windows.Forms.TextBox();
-            this.txtInssDemo = new System.Windows.Forms.TextBox();
-            this.txtIrDemo = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.txtSLDemo = new System.Windows.Forms.TextBox();
+            this.txtIrDemo = new System.Windows.Forms.TextBox();
+            this.txtInssDemo = new System.Windows.Forms.TextBox();
+            this.txtSBDemo = new System.Windows.Forms.TextBox();
+            this.lblSLDemo = new System.Windows.Forms.Label();
+            this.lblIrDemo = new System.Windows.Forms.Label();
+            this.lblInssDemo = new System.Windows.Forms.Label();
+            this.lblSBDemo = new System.Windows.Forms.Label();
             this.btnLimpar = new System.Windows.Forms.Button();
             this.btnVoltar = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
@@ -52,21 +52,21 @@ namespace Calculadora_IMC
             this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // lblSalarioBruto
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(18, 37);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Salário Bruto";
+            this.lblSalarioBruto.AutoSize = true;
+            this.lblSalarioBruto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalarioBruto.Location = new System.Drawing.Point(18, 37);
+            this.lblSalarioBruto.Name = "lblSalarioBruto";
+            this.lblSalarioBruto.Size = new System.Drawing.Size(116, 24);
+            this.lblSalarioBruto.TabIndex = 0;
+            this.lblSalarioBruto.Text = "Salário Bruto";
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.radioButton3);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.rbtClassC);
+            this.groupBox1.Controls.Add(this.rbtClassB);
+            this.groupBox1.Controls.Add(this.rbtClassA);
             this.groupBox1.Location = new System.Drawing.Point(17, 146);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(258, 166);
@@ -74,111 +74,54 @@ namespace Calculadora_IMC
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Cooperativa";
             // 
-            // radioButton1
+            // rbtClassC
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.Location = new System.Drawing.Point(5, 29);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(236, 28);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Classe A - 30% desconto";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.rbtClassC.AutoSize = true;
+            this.rbtClassC.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtClassC.Location = new System.Drawing.Point(5, 117);
+            this.rbtClassC.Name = "rbtClassC";
+            this.rbtClassC.Size = new System.Drawing.Size(236, 28);
+            this.rbtClassC.TabIndex = 2;
+            this.rbtClassC.TabStop = true;
+            this.rbtClassC.Text = "Classe C - 10% desconto";
+            this.rbtClassC.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // rbtClassB
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.Location = new System.Drawing.Point(5, 73);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(235, 28);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Classe B - 20% desconto";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.rbtClassB.AutoSize = true;
+            this.rbtClassB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtClassB.Location = new System.Drawing.Point(5, 73);
+            this.rbtClassB.Name = "rbtClassB";
+            this.rbtClassB.Size = new System.Drawing.Size(235, 28);
+            this.rbtClassB.TabIndex = 1;
+            this.rbtClassB.TabStop = true;
+            this.rbtClassB.Text = "Classe B - 20% desconto";
+            this.rbtClassB.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // rbtClassA
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton3.Location = new System.Drawing.Point(5, 117);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(236, 28);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Classe C - 10% desconto";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.rbtClassA.AutoSize = true;
+            this.rbtClassA.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtClassA.Location = new System.Drawing.Point(5, 29);
+            this.rbtClassA.Name = "rbtClassA";
+            this.rbtClassA.Size = new System.Drawing.Size(236, 28);
+            this.rbtClassA.TabIndex = 0;
+            this.rbtClassA.TabStop = true;
+            this.rbtClassA.Text = "Classe A - 30% desconto";
+            this.rbtClassA.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.txtSalarioBruto);
             this.groupBox2.Controls.Add(this.groupBox3);
             this.groupBox2.Controls.Add(this.groupBox1);
-            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.lblSalarioBruto);
             this.groupBox2.Location = new System.Drawing.Point(12, 12);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(760, 351);
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.txtSLDemo);
-            this.groupBox3.Controls.Add(this.txtIrDemo);
-            this.groupBox3.Controls.Add(this.txtInssDemo);
-            this.groupBox3.Controls.Add(this.txtSBDemo);
-            this.groupBox3.Controls.Add(this.label5);
-            this.groupBox3.Controls.Add(this.label4);
-            this.groupBox3.Controls.Add(this.label3);
-            this.groupBox3.Controls.Add(this.label2);
-            this.groupBox3.Location = new System.Drawing.Point(523, 19);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(231, 326);
-            this.groupBox3.TabIndex = 3;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Demonstração";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(15, 28);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 24);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Salário Bruto";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(15, 103);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 24);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "INSS";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(15, 178);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(27, 24);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "IR";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(15, 253);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(134, 24);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "Salário Líquido";
             // 
             // txtSalarioBruto
             // 
@@ -189,21 +132,30 @@ namespace Calculadora_IMC
             this.txtSalarioBruto.TabIndex = 4;
             this.txtSalarioBruto.TextChanged += new System.EventHandler(this.txtSalarioBruto_TextChanged);
             // 
-            // txtSBDemo
+            // groupBox3
             // 
-            this.txtSBDemo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSBDemo.Location = new System.Drawing.Point(19, 55);
-            this.txtSBDemo.Name = "txtSBDemo";
-            this.txtSBDemo.Size = new System.Drawing.Size(134, 29);
-            this.txtSBDemo.TabIndex = 5;
+            this.groupBox3.Controls.Add(this.txtSLDemo);
+            this.groupBox3.Controls.Add(this.txtIrDemo);
+            this.groupBox3.Controls.Add(this.txtInssDemo);
+            this.groupBox3.Controls.Add(this.txtSBDemo);
+            this.groupBox3.Controls.Add(this.lblSLDemo);
+            this.groupBox3.Controls.Add(this.lblIrDemo);
+            this.groupBox3.Controls.Add(this.lblInssDemo);
+            this.groupBox3.Controls.Add(this.lblSBDemo);
+            this.groupBox3.Location = new System.Drawing.Point(523, 19);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(231, 326);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Demonstração";
             // 
-            // txtInssDemo
+            // txtSLDemo
             // 
-            this.txtInssDemo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtInssDemo.Location = new System.Drawing.Point(19, 130);
-            this.txtInssDemo.Name = "txtInssDemo";
-            this.txtInssDemo.Size = new System.Drawing.Size(134, 29);
-            this.txtInssDemo.TabIndex = 8;
+            this.txtSLDemo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSLDemo.Location = new System.Drawing.Point(19, 280);
+            this.txtSLDemo.Name = "txtSLDemo";
+            this.txtSLDemo.Size = new System.Drawing.Size(134, 29);
+            this.txtSLDemo.TabIndex = 10;
             // 
             // txtIrDemo
             // 
@@ -213,13 +165,61 @@ namespace Calculadora_IMC
             this.txtIrDemo.Size = new System.Drawing.Size(134, 29);
             this.txtIrDemo.TabIndex = 9;
             // 
-            // txtSLDemo
+            // txtInssDemo
             // 
-            this.txtSLDemo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSLDemo.Location = new System.Drawing.Point(19, 280);
-            this.txtSLDemo.Name = "txtSLDemo";
-            this.txtSLDemo.Size = new System.Drawing.Size(134, 29);
-            this.txtSLDemo.TabIndex = 10;
+            this.txtInssDemo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInssDemo.Location = new System.Drawing.Point(19, 130);
+            this.txtInssDemo.Name = "txtInssDemo";
+            this.txtInssDemo.Size = new System.Drawing.Size(134, 29);
+            this.txtInssDemo.TabIndex = 8;
+            // 
+            // txtSBDemo
+            // 
+            this.txtSBDemo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSBDemo.Location = new System.Drawing.Point(19, 55);
+            this.txtSBDemo.Name = "txtSBDemo";
+            this.txtSBDemo.Size = new System.Drawing.Size(134, 29);
+            this.txtSBDemo.TabIndex = 5;
+            // 
+            // lblSLDemo
+            // 
+            this.lblSLDemo.AutoSize = true;
+            this.lblSLDemo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSLDemo.Location = new System.Drawing.Point(15, 253);
+            this.lblSLDemo.Name = "lblSLDemo";
+            this.lblSLDemo.Size = new System.Drawing.Size(134, 24);
+            this.lblSLDemo.TabIndex = 7;
+            this.lblSLDemo.Text = "Salário Líquido";
+            // 
+            // lblIrDemo
+            // 
+            this.lblIrDemo.AutoSize = true;
+            this.lblIrDemo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIrDemo.Location = new System.Drawing.Point(15, 178);
+            this.lblIrDemo.Name = "lblIrDemo";
+            this.lblIrDemo.Size = new System.Drawing.Size(27, 24);
+            this.lblIrDemo.TabIndex = 6;
+            this.lblIrDemo.Text = "IR";
+            // 
+            // lblInssDemo
+            // 
+            this.lblInssDemo.AutoSize = true;
+            this.lblInssDemo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInssDemo.Location = new System.Drawing.Point(15, 103);
+            this.lblInssDemo.Name = "lblInssDemo";
+            this.lblInssDemo.Size = new System.Drawing.Size(52, 24);
+            this.lblInssDemo.TabIndex = 5;
+            this.lblInssDemo.Text = "INSS";
+            // 
+            // lblSBDemo
+            // 
+            this.lblSBDemo.AutoSize = true;
+            this.lblSBDemo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSBDemo.Location = new System.Drawing.Point(15, 28);
+            this.lblSBDemo.Name = "lblSBDemo";
+            this.lblSBDemo.Size = new System.Drawing.Size(116, 24);
+            this.lblSBDemo.TabIndex = 4;
+            this.lblSBDemo.Text = "Salário Bruto";
             // 
             // btnLimpar
             // 
@@ -265,11 +265,11 @@ namespace Calculadora_IMC
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSalarioBruto;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton rbtClassC;
+        private System.Windows.Forms.RadioButton rbtClassB;
+        private System.Windows.Forms.RadioButton rbtClassA;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.TextBox txtSalarioBruto;
         private System.Windows.Forms.GroupBox groupBox3;
@@ -277,10 +277,10 @@ namespace Calculadora_IMC
         private System.Windows.Forms.TextBox txtIrDemo;
         private System.Windows.Forms.TextBox txtInssDemo;
         private System.Windows.Forms.TextBox txtSBDemo;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSLDemo;
+        private System.Windows.Forms.Label lblIrDemo;
+        private System.Windows.Forms.Label lblInssDemo;
+        private System.Windows.Forms.Label lblSBDemo;
         private System.Windows.Forms.Button btnLimpar;
         private System.Windows.Forms.Button btnVoltar;
     }
--- Calculadora IMC/frmFolhaPagamento.cs
+++ Calculadora IMC/frmFolhaPagamento.cs
@@ -34,10 +34,89 @@ namespace Calculadora_IMC
 
         private void txtSalarioBruto_TextChanged(object sender, EventArgs e)
         {
-            double salarioBruto, inss, ir, salarioLiquido;
+            double salarioBruto, inss = 0, inssTaxa = 0, ir = 0, coopTaxa = 0, salarioLiquido;
 
-            salarioBruto = Convert.ToDouble(txtSalarioBruto.Text);
+            double.TryParse(txtSalarioBruto.Text, out salarioBruto);
+
+                if (salarioBruto <= 1412)
+                {
+                    inss = 0;
+                }
+                else if (salarioBruto <= 2666.68)
+                {
+                    inss = 1;
+                }
+                else if (salarioBruto <= 4000.03)
+                {
+                    inss = 2;
+                }
+                else if (salarioBruto <= 7786.02)
+                {
+                    inss = 3;
+                }
+
+                switch (inss)
+                {
+                    case 0:
+                        inssTaxa = salarioBruto * 0.075;
+                        break;
+                    case 1:
+                        inssTaxa = salarioBruto * 0.09 - 21.18;
+                        break;
+                    case 2:
+                        inssTaxa = salarioBruto * 0.12 - 101.18;
+                        break;
+                    case 3:
+                        if (salarioBruto >= 7786.02)
+                        {
+                            inssTaxa = 908.86;
+                        }
+                        else
+                        {
+                            inssTaxa = salarioBruto * 0.14 - 181.18;
+                        }
+                        break;
+                }
+
+                if (salarioBruto <= 2112)
+                {
+                    ir = 0;
+                }
+                else if (salarioBruto <= 2826.65)
+                {
+                    ir = salarioBruto * 0.075;
+                }
+                else if (salarioBruto <= 4664.68)
+                {
+                    ir = salarioBruto * 0.225;
+                }
+                else if (salarioBruto >= 4664.69)
+                {
+                    ir = salarioBruto * 0.275;
+                }
+
+                if (rbtClassA.Checked == true)
+                {
+                    coopTaxa = salarioBruto * 0.3;
+                }
+                else if (rbtClassB.Checked == true)
+                {
+                    coopTaxa = salarioBruto * 0.2;
+                }
+                else if (rbtClassC.Checked == true)
+                {
+                    coopTaxa = salarioBruto * 0.1;
+                }
+
+                salarioLiquido = salarioBruto - inssTaxa - ir - coopTaxa;
+
+            txtSLDemo.Text = Convert.ToString(salarioLiquido);
+            txtIrDemo.Text = Convert.ToString(ir);
+            txtSBDemo.Text = txtSalarioBruto.Text;
+            txtInssDemo.Text = Convert.ToString(inssTaxa);
+
 
         }
+
     }
 }
